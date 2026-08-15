3cc29fdc5746251710594df38be0f61f26615cb0
rodolphovs <rodolphovs@gmail.com>
2026-08-15 20:17:27 -0300

    feat: add predation to simulation

--- src/pops.jl
+++ src/pops.jl
@@ -1,8 +1,12 @@
 using Plots
+using Random
+using Dates
 
-iterations = 5
-animal = "deer"
-fname = "data/pops.csv"
+predation_table = Dict(
+	"wolf"=>["rabbit", "deer"],
+	"bear"=>["deer", "wolf"])
+seed = convert(Int64, floor(datetime2unix(now())))
+rng = MersenneTwister(seed)
 
 mutable struct Cells
 	field::Vector{Vector{Dict{String, Int64}}}
@@ -50,6 +54,7 @@ function get_heatmap_plot(animal_matrix::Matrix{Int64}, animal::String)
 	    1:size(animal_matrix, 2), 
 	    animal_matrix,
 	    c=cgrad([:blue, :yellow, :red]),
+	    clim=(0,10),
 	    xlabel="x values", ylabel="y values",
 	    title=tittle)
 end
@@ -59,7 +64,7 @@ function random_walk(cells::Cells, animal::String, x::Int64, y::Int64)
 	size_x = cells.size_x
 	size_y = cells.size_y
 	for i = 1:animal_count
-		chance = rand(1:100)
+		chance = rand(rng, 1:100)
 		if chance > 0 && chance <= 20 && y + 1 <= size_y
 			cells.field[x][y][animal] = cells.field[x][y][animal] - 1
 			cells.field[x][y + 1][animal] = cells.field[x][y + 1][animal] + 1 
@@ -78,19 +83,52 @@ end
 
 function reproduce(cells::Cells, animal::String, x::Int64, y::Int64)
 	animal_count = cells.field[x][y][animal]
-	chance = rand(1:100)
-	if animal_count > 1 && chance < 60
+	chance = rand(rng, 1:100)
+	if animal_count > 1 && chance < 80
 		cells.field[x][y][animal] += 1
 	end
 end
 
 function die_of_overpopulation(cells::Cells, animal::String, x::Int64, y::Int64)
 	animal_count = cells.field[x][y][animal]
-	if animal_count > 10 && chance < 50
-		cells.field[x][y][animal] -= 4
-		if cells.field[x][y][animal] < 0
-			cells.field[x][y][animal] = 0
+	chance = rand(rng, 1:100)
+	if animal_count > 6 && chance < 80
+		cells.field[x][y][animal] = floor(cells.field[x][y][animal] / 2)
+	end
+end
+
+function get_prey_amount(cells::Cells, animal::String, x::Int64, y::Int64)
+	prey_amount = 0
+	if !(animal in keys(predation_table))
+		return prey_amount
+	end
+	prey_types = predation_table[animal]
+	for prey_type in prey_types
+		if prey_type in cells.animals
+			prey_amount += cells.field[x][y][prey_type]
 		end
+	end
+	return prey_amount
+end
+
+function predate(cells::Cells, animal::String, x::Int64, y::Int64)
+	if !(animal in keys(predation_table))
+		return
+	end
+	target_predation = log2(cells.field[x][y][animal])
+	for _ in 1:target_predation
+		prey_amount = get_prey_amount(cells, animal, x, y)
+		if prey_amount <= 0
+			break
+		end
+		prey_types_in_field = filter((a)->
+			a in cells.animals 
+				&& cells.field[x][y][a] > 0,
+			predation_table[animal])
+
+		choice = rand(rng, 1:size(prey_types_in_field, 1))
+		prey_choice = prey_types_in_field[choice]
+		cells.field[x][y][prey_choice] -= 1
 	end
 end
 
@@ -103,6 +141,7 @@ function simulate_animal(cells::Cells, animal::String, x::Int64, y::Int64)
 	end
 
 	random_walk(cells, animal, x, y)
+	predate(cells, animal, x, y)
 	reproduce(cells, animal, x, y)
 	die_of_overpopulation(cells, animal, x, y)
 end
@@ -117,7 +156,7 @@ function simulate(cells::Cells, size_x::Int64, size_y::Int64)
 	end
 end
 
-function read_pops_csv()
+function read_pops_csv(fname::String)
 	f = open(fname, "r")
 	animals_in_cell = Vector{AnimalInCell}()
 	lines = readlines(f)
@@ -135,7 +174,7 @@ function read_pops_csv()
 	end
 	close(f)
 	
-	animals = map((animals_in_cell)->animals_in_cell.animal, animals_in_cell)
+	animals = unique(map((animals_in_cell)->animals_in_cell.animal, animals_in_cell))
 	cells = empty_cells(size_x, size_y, animals)
 
 	for animal_in_cell in animals_in_cell
@@ -147,15 +186,47 @@ function read_pops_csv()
 	return cells
 end
 
-cells = read_pops_csv()
-
-for i = 1:iterations
-	simulate(cells, cells.size_x, cells.size_y)
-
-	plot_matrix = animal_matrix_to_plot(cells, animal)
-	ht = get_heatmap_plot(plot_matrix, animal)
-
-	println("Plotting ", animal)
-	display(ht)
-	readline()
+function read_conf(fname::String)
+	f = open(fname, "r")
+	lines = readlines(f)
+	conf = Dict{String, String}()
+	for line in lines
+		values = split(line, "=")
+		key = string(values[1])
+		value = string(values[2])
+		conf[key] = value
+	end
+	close(f)
+	return conf
 end
+
+function main()
+	# configs
+	conf = read_conf("simulation.conf")
+	iterations = parse(Int64, conf["iterations"])
+	time_per_frame = parse(Float64, conf["time_per_frame"])
+	animal_to_plot = conf["animal_to_plot"]
+	initial_pop_csv = conf["initial_pop_csv"]
+	
+	#script
+	cells = read_pops_csv(initial_pop_csv)
+	if !(animal_to_plot in cells.animals)
+		throw(DomainError("Animal '"*animal_to_plot*"' not in field: "*string(cells.animals)))
+	end
+	println("Plotting ", animal_to_plot)
+	for i = 1:iterations
+		println("Iteration "*string(i))
+		plot_matrix = animal_matrix_to_plot(cells, animal_to_plot)
+		ht = get_heatmap_plot(plot_matrix, animal_to_plot)
+		display(ht)
+		sleep(time_per_frame)
+		simulate(cells, cells.size_x, cells.size_y)
+	end
+
+	plot_matrix = animal_matrix_to_plot(cells, animal_to_plot)
+	ht = get_heatmap_plot(plot_matrix, animal_to_plot)
+	display(ht)
+	sleep(5)
+end
+
+main()
